1fd4dd26d2a8bb4082d7f6a5d2eb6fe1e610526a
Luan Caetano <luanlcr@hotmail.com>
2021-10-03 11:35:23 -0300

    Data Annotations de Produto

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Quiron.LojaVirtual.Dominio.Entidades
 {
+    [Table("Produtos")]
     public class Produto
     {
+        [Key]
         public int ProdutoId { get; set; }
+
+        [Required]
         public string Nome { get; set; }
+
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
-        public decimal Preco { get; set; }
+
+        [Required]
+        [Display(Name = "Preço")]
+        public decimal Preco { get; set; } = 0.0m;
+
+        [Required]
         public string Categoria { get; set; }
     }
 }
