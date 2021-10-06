c4ece255b4e34eeefcc51f1aa06227dc32d1945b
Luan Caetano <luanlcr@hotmail.com>
2021-10-06 19:09:59 -0300

    LojaContext, StringConnection

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/LojaContext.cs
@@ -0,0 +1,20 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class LojaContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        public LojaContext() : base("Loja")
+        {
+            Database.CreateIfNotExists();
+        }
+    }
+}
