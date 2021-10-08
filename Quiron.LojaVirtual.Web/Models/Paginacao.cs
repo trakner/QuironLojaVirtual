a31e2b98abe6b41af46dfe6ffd31c7f1418ae19a
Luan Caetano <luanlcr@hotmail.com>
2021-10-08 19:32:14 -0300

    Classes de Paginacao

---
+++ Quiron.LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Quiron.LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        public int ItensTotal { get; set; }
+
+        public int ItensPorPagina { get; set; }
+
+        public int PaginaAtual { get; set; }
+
+        public int TotalDePaginas
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)ItensTotal / ItensPorPagina);
+            } 
+        }
+    }
+}
